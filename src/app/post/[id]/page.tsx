a2b95c5044e1ee193c45038d188b2810b1169ccd
CarSeP <vjpd64@gmail.com>
2025-07-15 08:23:17 -0400

    Move post page

--- src/app/post/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-function PostPage() {
-  return <h1> Post</h1>;
-}
-
-export default PostPage;
---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import PostDetail from "@/components/PostDetail/PostDetail";
+
+function PostPage() {
+  return <PostDetail />;
+}
+
+export default PostPage;
