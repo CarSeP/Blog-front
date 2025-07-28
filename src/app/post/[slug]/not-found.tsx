2e01970f72cf77567b0cb45fde02dbd4e27b5ee1
CarSeP <vjpd64@gmail.com>
2025-07-28 12:03:53 -0400

    Implement 404 view for post not found

---
+++ src/app/post/[slug]/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFoundCard from "@/components/NotFoundCard/NotFoundCard";
+
+export default function NotFound() {
+  return (
+    <NotFoundCard
+      title=""
+      subTitle="Post no encontrado"
+      description="Lo sentimos, el post que estás buscando no existe o ha sido eliminado."
+    />
+  );
+}
