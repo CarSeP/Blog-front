9f8ca5bdbbb321e95843045958b111c05aaa58f2
CarSeP <vjpd64@gmail.com>
2025-08-11 22:33:57 -0400

    Add login button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,14 @@ import style from "./Header.module.css";
 export function Header() {
   return (
     <header className={style.header}>
-      <h1 className={style.title}><Link href="/">Blog</Link></h1>
+      <div className={style.headerContainer}>
+        <h1 className={style.title}>
+          <Link href="/">Blog</Link>
+        </h1>
+        <Link href="/login">
+          <button className={style.loginButton}>Login</button>
+        </Link>
+      </div>
     </header>
   );
 }
