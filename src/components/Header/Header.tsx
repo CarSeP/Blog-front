eba785605f3663a501612811c24d60b3b3ce9a97
CarSeP <vjpd64@gmail.com>
2025-08-26 12:17:21 -0400

    Implement icon button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,26 @@
-import Link from "next/link";
 import style from "./Header.module.css";
+import { cookies } from "next/headers";
+import Link from "next/link";
+import HeaderButton from "../HeaderButton/HeaderButton";
+import jwt from "jsonwebtoken";
 
-export function Header() {
+export async function Header() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("authToken")?.value || "";
+  let isAuth = "";
+  jwt.verify(token, process.env.JWT_SECRET || "", function (err, decoded) {
+    if (decoded && typeof decoded !== "string") {
+      isAuth = decoded.id;
+    }
+  });
+
   return (
     <header className={style.header}>
       <div className={style.headerContainer}>
         <h1 className={style.title}>
           <Link href="/">Blog</Link>
         </h1>
-        <Link href="/login">
-          <button className={style.loginButton}>Login</button>
-        </Link>
+        <HeaderButton isAuth={isAuth} />
       </div>
     </header>
   );
