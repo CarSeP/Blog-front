54e5ed754188f080d4fb92a2f146d2fd1b312611
Carlos José <157814287+CarSeP@users.noreply.github.com>
2026-02-15 16:34:27 -0400

    Feat/20 añadir funcionalidad de crear post (#21)
    * Rename component
    
    * Update imports
    
    * Add hidden class
    
    * Add categories responsive funcionality
    
    * rename classes
    
    * Add login button
    
    * Create Login page and Login component
    
    * Install packages
    
    * Edit shcema.prisma
    
    * Add new migration
    
    * Create login page and login component
    
    * Create login endpoint
    
    * Connect the login with the login endpoint
    
    * Fix loginSection height
    
    * Change placeholders
    
    * Update schema.prisma
    
    * Install new package
    
    * Add migration
    
    * Create registe endpoint
    
    * Create Register component
    
    * Add pagination on post endpoint
    
    * Create page interface
    
    * Create Pagination component
    
    * Implement pagination
    
    * Add filter in the post api
    
    * Implement filter
    
    * Modify login endpoint
    
    * Implement icon button
    
    * Add logout endpoint
    
    * Implement logout funcionality
    
    * Add post endpoint
    
    * Add create post button
    
    * Create PostUpsert page

---
+++ src/app/post/create/page.tsx
@@ -0,0 +1,19 @@
+import PostUpsert from "@/components/PostUpsert/PostUpsert";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+
+async function CreatePostPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("authToken")?.value || "";
+  let username = "";
+  jwt.verify(token, process.env.JWT_SECRET || "", function (err, decoded) {
+    if (decoded && typeof decoded !== "string") {
+      username = decoded.id;
+    }
+  });
+
+
+  return <PostUpsert authorUsername={username}/>;
+}
+
+export default CreatePostPage;
